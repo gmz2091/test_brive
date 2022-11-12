884d7b5ecc95bc511a16ee05d03e51c5f1740122
Magdiel <mgomez2091@gmail.com>
2022-11-11 23:35:00 -0700

    add user if none exists

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Box } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import React, { useState} from 'react';
 import {UserItem} from './components';
 import {ModalView, ModalForm} from './components';
+import { ContainerActions } from './components/UserItem/style';
 import useUser from './hooks/useUser';
 import { User } from './interfaces/user.interface';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import { setModalData } from './redux/reducers/modalDataReducer';
+import { useAppDispatch } from './redux/store/hooks';
 
 function App() {
+  const dispatch = useAppDispatch()
   const {data, loading} = useUser();
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -22,7 +27,23 @@ function App() {
     <ModalForm openForm={openForm} handleCloseForm={handleCloseForm} />
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', maxWidth: 375, margin: 'auto', padding: 2 }}>
       {loading && 'Loading...'}
-      {data.length === 0 && !loading && <p>No users found</p>}
+      {data.length === 0 && !loading && (
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', maxWidth: 375, margin: 'auto', padding: 2 }}>
+          <h1>No hay usuarios, agrega nuevo usuario</h1>
+          <ContainerActions>
+          <IconButton onClick={() => {
+            dispatch(setModalData({
+              title: 'Agregar usuario',
+              btnText: 'Agregar',
+              addUser: true,
+            }))
+            handleOpenForm()
+            }} aria-label="person-add" color="primary">
+            <PersonAddIcon />
+          </IconButton>
+        </ContainerActions>
+        </Box>
+      )}
       {loading ? 'Loading...' : data?.map((user: User) => 
         <UserItem 
           key={user.id}
